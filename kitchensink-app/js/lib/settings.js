ae150bf9246576d7d43f922a4c84fb58701e3411
Piotr Zalewa <piotr@zalewa.info>
2013-05-03 16:53:12 +0200

    * settings is showing up as a dark modal * switching certified from the settings page * set the default state * automatically update setting elements if chosen

--- kitchensink-app/js/lib/settings.js
+++ kitchensink-app/js/lib/settings.js
@@ -1,36 +1,81 @@
 define(function(require) {
   var prime = require('prime');
   var Emitter = require('prime/emitter');
+  var $ = require('elements');
+  require('elements/events');
+  require('elements/attributes');
+  require('elements/traversal');
   
   var Settings = prime({
     inherits: Emitter,
 
     options: {
-      'certifiedVisible': false
+      certifiedVisible: {
+        // do not display certified apps by default
+        value: false,
+        // update setting from element
+        updateFrom: 'certifiedVisible',
+        isBoolean: true
+      }
     },
 
     constructor: function() {
       // update settings from localStorage
       var localSettings = JSON.parse(localStorage.settings || '{}');
       for (var key in localSettings) {
-        this.options[key] = localSettings[key];
+        this.options[key].value = localSettings[key];
       } 
       this.store();
+      this.updateElements();
     },
 
     get: function(key) {
-      return this.options[key];
+      return this.options[key].value;
     },
 
     set: function(key, value) {
-      this.options[key] = value;
+      this.options[key].value = value;
       this.store();
       this.emit(key);
     },
 
     store: function() {
-      localStorage.settings = JSON.stringify(this.options);
-    }
+      var values = {};
+      for (var key in this.options) {
+        values[key] = this.options[key].value;
+      }
+      localStorage.settings = JSON.stringify(values);
+    },
+
+    /**
+     * update current state of elements 
+     */
+    updateElements: function() {
+      var self = this;
+      for (var key in this.options) {
+        var item = this.options[key];
+        if (item.updateFrom) {
+          var element = $('#' + item.updateFrom);
+          if (item.isBoolean) {
+            if (item.value) {
+              element.check();
+            } else {
+              element.uncheck();
+            }
+            // hook to change event
+            element.on('change', function() {
+              self.set(key, this.checked());
+            });
+          } else {
+            element.value = item.value;
+            // hook to change event
+            element.on('change', function() {
+              self.set(key, this.value);
+            });
+          }
+        }
+      }
+    },
 
   });
 
